87ad6e5ab9e27550022464bb88a32466f7260264
Priscilla Rebouças <priscillabreboucas@gmail.com>
2022-06-27 16:17:17 +0200

    Adding date count

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const prompt = require('prompt-sync')();
 
-function greet(namePassedIn) {
-  return 'Welcome to SALT, ';
+function greet(namePassedIn, countDate) {
+  return `
+  Welcome to SALT, ${namePassedIn}
+  Today it is ${countDate} left until the course starts`;
 }
 
 // Main program
 const name = prompt('What is your name? ');
-const greeting = greet(name);
+const init = prompt('When does the course start? ');
+const future = new Date(init);
+const current = new Date();
+
+const diff = Math.abs(future.getTime() - current.getTime());
+const days = Math.ceil(diff/(1000 * 60 * 60 * 24));
+
+const greeting = greet(name, days);
 console.log(greeting);
 
 module.exports.greet = greet;
